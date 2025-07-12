37ad3a4a4dc61de372f435865155953649242e93
Ken9903 <godsdy0410@naver.com>
2025-07-12 15:09:31 +0900

    프로젝트 정리

---
+++ AR_Caps/Assets/Main/QRCodeSupporter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using QRFoundation;
+
+
+public class QRCodeSupporter : MonoBehaviour
+{
+    public GameObject ame;
+    public GameObject isu;
+    public GameObject undou;
+    public GameObject egaki;
+    public GameObject ongaku;
+}
